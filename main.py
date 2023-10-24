512381fccb99c88d69821978a4862f04493c0ef1
Vovchik777 <vnovoid@gmail.com>
2023-10-24 23:51:48 +0300

    123

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import pygame
 from configs import *
 
 pygame.init()
-screen = pygame.display.set_mode((ScreenWidth,ScreenHeight))
+screen = pygame.display.set_mode((ScreenWidth, ScreenHeight))
 pygame.display.set_caption('Игрушка')
 clock = pygame.time.Clock()
 font = pygame.font.SysFont('couriernew', 40)
-text = font.render(str('Папа, тебе пора спать!'), True, (255,255,0))
+text = font.render(str('Папа, тебе пора спать!'), True, (255, 255, 0))
 textHeight = text.get_height()
 textWidth = text.get_width()
+
+
 def screen_draw():
-    screen.fill((0,0,20))
-    screen.blit(text, (ScreenWidth//2-textWidth//2,ScreenHeight//2-textHeight//2))
+    screen.fill((0, 0, 20))
+    screen.blit(text, (ScreenWidth // 2 - textWidth // 2, ScreenHeight // 2 - textHeight // 2))
     pygame.display.flip()
+
 
 while True:
     for event in pygame.event.get():
