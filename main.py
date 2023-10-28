9e25000c940b806bdf305a3a24fe85c47a10052c
Vovchik777 <vnovoid@gmail.com>
2023-10-28 19:37:37 +0300

    min max bot

--- main.py
+++ main.py
@@ -90,6 +90,12 @@ class Text:
         self.text_write = self.font.render(self.text, True, self.text_color)
 
 
+    def minus(self):
+
+        self.text = str(int(self.text) - 1)
+        self.text_write = self.font.render(self.text, True, self.text_color)
+
+
 start_button_play = Button(screen, (255, 125, 255), "Играть", (0, 130, 60), font_name='arial', border_radius=15)
 
 start_button_exit = Button(screen, (255, 0, 125), "Выход", (0, 125, 0), font_size=20, border_radius=15)
@@ -97,20 +103,30 @@ start_button_exit = Button(screen, (255, 0, 125), "Выход", (0, 125, 0), fon
 config_button_back = Button(screen, (255, 255, 0), "", (0, 125, 0), font_size=20, image_file_name='button_back.png',
                             border_radius=10)
 
-bot_plus = Button(screen, (255, 255, 0), "", (0, 125, 0), font_size=20, image_file_name='plus.png',
+bot_plus = Button(screen, (0, 255, 0), "", (0, 125, 0), font_size=20, image_file_name='plus.png',
+                  border_radius=10)
+bot_minus = Button(screen, (255, 0, 0), "", (0, 125, 0), font_size=20, image_file_name='minus.png',
                   border_radius=10)
 
 config_button_back.move(20, 20)
 
 text_bot = Text("5", (255, 0, 255), font_size=40)
 
+button_start_game = Button(screen, (255, 125, 255), "Играть", (0, 130, 60), font_name='arial',border_radius=5)
 
+menu_cofigure_bot = Text('Количество ботов',(255,123,0))
+
+text_max = Text('Максимальное количество ботов!',(0,255,0),font_size= 30)
+max = False
+text_min = Text('Минимальное количество ботов!',(0,255,0),font_size= 30)
+min = False
 # 441 280
 
 
 def screen_draw():
-    screen.fill((0, 0, 255))
+    screen.fill((0,0,0))
     if scene == 'start':
+        screen.fill((0, 0, 255))
         start_button_play.draw()
         start_button_play.move(screen.get_width() // 2 - start_button_play.get_width() // 2,
                                screen.get_height() // 2 - start_button_play.get_height() // 2)
@@ -118,13 +134,26 @@ def screen_draw():
         start_button_exit.move(screen.get_width() // 2 - start_button_exit.get_width() // 2,
                                screen.get_height() // 2 - start_button_exit.get_height() // 2 + 100)
     if scene == 'configure_game':
+        screen.fill((123,87,0))
         bot_plus.draw()
+        bot_minus.draw()
+        menu_cofigure_bot.write()
+        menu_cofigure_bot.move(screen.get_width()//2 - menu_cofigure_bot.get_width()//2, screen.get_height()//3-menu_cofigure_bot.get_height()-100)
         config_button_back.draw()
         bot_plus.move(screen.get_width() // 2 + 50, screen.get_height() // 2 - 20)
+        bot_minus.move(screen.get_width() // 2 - 100, screen.get_height() // 2 - 20)
         text_bot.write()
         text_bot.move(screen.get_width() // 2 - text_bot.get_width() // 2,
                       screen.get_height() // 2 - text_bot.get_height() // 2)
+        button_start_game.draw()
+        button_start_game.move(screen.get_width()//2 - button_start_game.get_width()//2,screen.get_height()//2+button_start_game.get_height())
+        if max:
+            text_max.move(screen.get_width() // 2 - text_max.get_width()//2, screen.get_height() // 4 )
+            text_max.write()
 
+        if min:
+            text_min.move(screen.get_width() // 2 - text_max.get_width()//2, screen.get_height() // 4 )
+            text_min.write()
     pygame.display.flip()
 
 
@@ -149,8 +178,20 @@ while True:
             if scene == 'configure_game':
                 if config_button_back.is_pressed(mpos):
                     scene = 'start'
-                if bot_plus.is_pressed(mpos) and int(text_bot.get_value()) < 15:
-                    text_bot.add()
+                if bot_plus.is_pressed(mpos):
+                    if int(text_bot.get_value()) < 15:
+                        text_bot.add()
+                        min = False
 
+                    else:
+                        max = True
+                if bot_minus.is_pressed(mpos):
+                    if int(text_bot.get_value()) > 5:
+                        text_bot.minus()
+                        max=False
+                    else:
+                        min = True
+                if button_start_game.is_pressed(mpos):
+                    scene = 'game'
     screen_draw()
     clock.tick(FPS)
